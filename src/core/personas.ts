196cc6945e30e2fd16e0e26269154391e6ea96b3
Beau Lyddon <lyddonb@gmail.com>
2024-02-19 16:35:53 -0700

    Remove unneeded code

--- src/core/personas.ts
+++ src/core/personas.ts
@@ -1,15 +1,4 @@
-import { postJson } from "@/http"
-
 export interface Persona {
   title: string
   description: string
-}
-
-export const getPersonas = async (question: string) => {
-  const url = "api/personas"
-  const payload = {
-    question: question,
-  }
-  const result = await postJson(url, payload)
-  return (await result.json()) as Persona[]
 }
